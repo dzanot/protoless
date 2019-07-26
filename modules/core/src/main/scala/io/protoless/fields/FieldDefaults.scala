458dd28d49ad27149e8da37e7055dab357b95b77
Davis Zanot <davis.zanot@47deg.com>
2019-07-26 13:56:29 -0700

    fix anyval defaults

--- modules/core/src/main/scala/io/protoless/fields/FieldDefaults.scala
+++ modules/core/src/main/scala/io/protoless/fields/FieldDefaults.scala
@@ -20,37 +20,65 @@ final object FieldDefault {
   override val default: A = v
  }
 
- implicit final val intDefault = defaultOf[Int](0)
- implicit final val uintDefault = defaultOf[Int @@ Unsigned](tag[Unsigned][Int](0))
- implicit final val sintDefault = defaultOf[Int @@ Signed](tag[Signed][Int](0))
- implicit final val fintDefault = defaultOf[Int @@ Fixed](tag[Fixed][Int](0))
- implicit final val sfintDefault = defaultOf[Int @@ Signed with Fixed](tag[Signed with Fixed][Int](0))
+ implicit final val intDefault: FieldDefault[Int] =
+  defaultOf[Int](0)
+ implicit final val uintDefault: FieldDefault[Int @@ Unsigned] =
+  defaultOf[Int @@ Unsigned](tag[Unsigned][Int](0))
+ implicit final val sintDefault: FieldDefault[Int @@ Signed] =
+  defaultOf[Int @@ Signed](tag[Signed][Int](0))
+ implicit final val fintDefault: FieldDefault[Int @@ Fixed] =
+  defaultOf[Int @@ Fixed](tag[Fixed][Int](0))
+ implicit final val sfintDefault: FieldDefault[Int @@ Signed with Fixed] =
+  defaultOf[Int @@ Signed with Fixed](tag[Signed with Fixed][Int](0))
 
- implicit final val longDefault = defaultOf[Long](0)
- implicit final val ulongDefault = defaultOf[Long @@ Unsigned](tag[Unsigned][Long](0))
- implicit final val slongDefault = defaultOf[Long @@ Signed](tag[Signed][Long](0))
- implicit final val flongDefault = defaultOf[Long @@ Fixed](tag[Fixed][Long](0))
- implicit final val sflongDefault = defaultOf[Long @@ Signed with Fixed](tag[Signed with Fixed][Long](0))
+ implicit final val longDefault =
+  defaultOf[Long](0)
+ implicit final val ulongDefault =
+  defaultOf[Long @@ Unsigned](tag[Unsigned][Long](0))
+ implicit final val slongDefault =
+  defaultOf[Long @@ Signed](tag[Signed][Long](0))
+ implicit final val flongDefault =
+  defaultOf[Long @@ Fixed](tag[Fixed][Long](0))
+ implicit final val sflongDefault =
+  defaultOf[Long @@ Signed with Fixed](tag[Signed with Fixed][Long](0))
 
- implicit final val booleanDefault = defaultOf[Boolean](false)
- implicit final val doubleDefault = defaultOf[Double](0.0)
- implicit final val floatDefault = defaultOf[Float](0.0f)
- implicit final val stringDefault = defaultOf[String]("")
- implicit final val uuidDefault = defaultOf[UUID](UUID.nameUUIDFromBytes(Array()))
- implicit final val byteStringDefault = defaultOf[ByteString](ByteString.EMPTY)
+ implicit final val booleanDefault =
+  defaultOf[Boolean](false)
 
- implicit final val shortDefault = defaultOf[Short](0)
- implicit final val bigIntDefault = defaultOf[BigInt](BigInt(0))
- implicit final val bigDecimalDefault = defaultOf[BigDecimal](BigDecimal(0))
+ implicit final val doubleDefault =
+  defaultOf[Double](0.0)
 
- implicit final val charDefault = defaultOf[Char](0)
+ implicit final val floatDefault =
+  defaultOf[Float](0.0f)
 
+ implicit final val stringDefault =
+  defaultOf[String]("")
+
+ implicit final val uuidDefault =
+  defaultOf[UUID](UUID.nameUUIDFromBytes(Array()))
+
+ implicit final val byteStringDefault =
+  defaultOf[ByteString](ByteString.EMPTY)
+
+ implicit final val shortDefault =
+  defaultOf[Short](0)
+
+ implicit final val bigIntDefault =
+  defaultOf[BigInt](BigInt(0))
+
+ implicit final val bigDecimalDefault =
+  defaultOf[BigDecimal](BigDecimal(0))
+
+ implicit final val charDefault =
+  defaultOf[Char](0)
 
  implicit def valueClassDefault[A <: AnyVal, V](implicit
                                                 ev: A <:< AnyVal,
-                                                v: FieldDefault[V],
-                                                unwrapped: Unwrapped.Aux[A, V]): FieldDefault[A] = new FieldDefault[A] {
-  override val default: A = ev(default).asInstanceOf[A]
+                                                unwrapped: Unwrapped.Aux[A, V],
+                                                v: FieldDefault[V]
+                                                ): FieldDefault[A] = new FieldDefault[A] {
+  override val default: A = unwrapped.wrap(v.default)
+
  }
 
  implicit def listDefault[A]: FieldDefault[List[A]] = new FieldDefault[List[A]] {
@@ -60,5 +88,4 @@ final object FieldDefault {
  implicit def optionDefault[A]: FieldDefault[Option[A]] = new FieldDefault[Option[A]] {
   override val default: Option[A] = None
  }
-
 }
